9a801837a8c100e4fcf7950009ebb85fa95dacfe
0x5fe6eb50c7b537a9 <dan.decapria@gmail.com>
2015-01-04 20:47:23 -0500

    Additional program bootstraps (partial).

---
+++ initialization/bootstrap_programs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# list of Ubuntu 14.04 common PPA programs to install
+
+# versioning control systems
+apt-get install git subversion subversion-tools apache2 libapache2-svn git-svn
+
+# maven3, ant
+apt-get install maven ant
+
+# mysql
+apt-get install mysql-client-5.6 mysql-server-5.6 mydsql-common-5.6
+
+# mysql workbench
+
+
+
+
