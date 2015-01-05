2497f4eb40f36799315f52b9ccbf77aae840a932
0x5fe6eb50c7b537a9 <dan.decapria@gmail.com>
2015-01-05 11:43:02 -0500

    Updates to install script.

--- initialization/bootstrap_programs.sh
+++ initialization/bootstrap_programs.sh
@@ -4,13 +4,23 @@
 # versioning control systems
 apt-get install git subversion subversion-tools apache2 libapache2-svn git-svn
 
+# generic programs
+apt-get install emacs24-nox locate
+
+# php5 
+apt-get install php5 php5-cli php5-mysql php5-xdebug php-pear phpunit phpunit-story phpunit-selenium php-invoker javascript-common
+pear install PHP_CodeSniffer
+wget https://phar.phpunit.de/phpunit-skelgen.phar -P /tmp/
+chmod +x /tmp/phpunit-skelgen.phar
+mv /tmp/phpunit-skelgen.phar /usr/local/bin/phpunit-skelgen
+
 # maven3, ant
 apt-get install maven ant
 
 # mysql
-apt-get install mysql-client-5.6 mysql-server-5.6 mydsql-common-5.6
+apt-get install mysql-client-5.6 mysql-server-5.6 mysql-common-5.6
 
-# mysql workbench
+# outside: chrome, mysql workbench, Rstudio
 
 
 
